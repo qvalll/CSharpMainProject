cd65ca9fe00014232abdd4a81b6b4907213ee61b
qvall <qvall44@gmail.com>
2025-07-09 18:30:21 +0300

    commit12
    HomeTask_2

---
+++ Assets/Scripts/lmaoScript.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lmaoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // string lmao1 = "wasap ma boy";
+        // Debug.Log(lmao1);
+
+        //int a = 2;
+        //int a2 = 1;
+        //int a3 = a - a2;
+        //float b = 2.6f;
+        //string gg = "6";
+        //bool c = true;
+        //Debug.Log(10 / a3);
+
+        //Debug.Log("ave");
+
+        int number_1 = 5;
+        float number_2 = 3;
+
+        //Debug.Log(number_1 + number_2);
+        //Debug.Log(number_1 - number_2);
+        //Debug.Log(number_1 * number_2);
+        //Debug.Log(number_1 / number_2);
+        //Debug.Log(number_1 % number_2);
+        //Debug.Log((number_1 + number_2) * number_2);
+
+        //bool isEqual = number_1 == number_2;
+        //bool isNotEqual = number_1 != number_2;
+        //bool isLessThan = number_1 < number_2;
+        //bool isGreaterThan = number_1 > number_2;
+        //bool isLessThanOrEqual = number_1 <= number_2;
+        //bool isGreaterThanOrEqual = number_1 >= number_2;
+
+
+        //Debug.Log(isEqual);
+        //Debug.Log(isNotEqual);
+        //Debug.Log(isLessThan);
+        //Debug.Log(isGreaterThan);
+        //Debug.Log(isLessThanOrEqual);
+        //Debug.Log(isGreaterThanOrEqual);
+
+
+        //bool isTrue = true;
+        //bool isTrue1 = true;
+        //bool isFalse = false;
+
+        //bool and = isTrue && isFalse && isTrue1;
+
+        //bool or = isTrue || isFalse;
+
+        //bool not = !isTrue;
+
+        //Debug.Log(not);
+
+
+        //int time = 14;
+        //bool isWeekend = false;
+        //bool isEvening = (time >= 17);
+        //bool haveTicket = true;
+        //bool goToCinema = (isWeekend || isEvening) && haveTicket;
+
+        //Debug.Log(goToCinema);
+
+
+        int x = 10, y = 9, z = 8;
+        x += 5;
+        y -= 5;
+        z *= 5;
+
+        x++;
+
+
+
+        
+
+
+
+
+    }
+}
